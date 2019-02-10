d5377a993a2529f8d38b6a6c603a3a54d2888e01
unknown <orthogonaltrajectory@gmail.com>
2019-02-09 22:51:37 -0900

    Chat box template

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,7 +1,28 @@
 <template>
     <div class="chat container">
         <!-- this.name is the prop not the attribute in default -->
-        <h2>Chat {{this.name}}</h2>
+        <h2 class="center cyan-text">Chatting as {{this.name}}</h2>
+        <div class="card">
+            
+            <!-- Sender Data -->
+            <div class="card-content">
+                <!-- Messages are stored in an unordered list -->
+                <!-- Each message is a list item -->
+                <ul class="messages">
+                    <li>
+                        <span class="green-text">Name</span> 
+                        <span class="grey-text text-darken-3">Message</span>
+                        <span class="grey-text time">Time</span>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- New Message -->
+            <div class="card-action">
+                <input type="text">
+            </div>
+
+        </div>
     </div>
 </template>
 
@@ -18,7 +39,20 @@ export default {
 </script>
 
 <style>
+.chat h2 {
+    font-size: 2.6em;
+    margin-bottom: 40px;
+}
 
+.chat span {
+    font-size: 1.4em;
+}
+
+.chat .time {
+    /* Same line */
+    display: block;
+    font-size: 1.2em;
+}
 </style>
 
 
